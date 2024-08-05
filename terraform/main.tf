34a5d69f8269ff010aaf1db636ad83c4573eea0f
root <root@DESKTOP-OGLE4UA>
2024-08-05 11:35:56 +0200

    Update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "inchcape-rg"
+  name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "inchcape-asp"
+resource "azurerm_service_plan" "example" {
+  name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku {
@@ -18,14 +18,13 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "inchcapre-service"
+  name                = "example-appservice"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  service_plan_id     = azurerm_service_plan.example.id
 
   site_config {
     app_command_line = "node src/index.js"
-    node_version     = "~14"
   }
 
   app_settings = {
