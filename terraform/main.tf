214ca01b6e8fcb0b6933d0109019e97342cdfd15
Earl Sioson <sioson.emc@gmail.com>
2024-08-05 12:04:18 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "inchcape-rg"
   location = "West Europe"
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example-appserviceplan"
+  name                = "inchcape-asp"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type             = "Linux"
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "example-appservice"
+  name                = "inchcape-app"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   service_plan_id     = azurerm_service_plan.example.id
