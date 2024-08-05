fec20a69c5c5c92e3ef7aaa6d100f9e3fb80be5a
Earl Sioson <sioson.emc@gmail.com>
2024-08-05 14:03:31 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.52.0"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
