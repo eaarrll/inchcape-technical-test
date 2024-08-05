39d2358e260c1bb252ebd62a20e969f2ec71d5f3
Earl Sioson <sioson.emc@gmail.com>
2024-08-05 12:03:41 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "inchcape-rg"
+  name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "inchcape-asp"
+  name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type             = "Linux"
@@ -16,13 +16,15 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "inchcape-app"
+  name                = "example-appservice"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_service_plan.example.id
+  service_plan_id     = azurerm_service_plan.example.id
 
   site_config {
     app_command_line = "node src/index.js"
+    always_on = true
+    linux_fx_version = "NODE|14-lts"
   }
 
   app_settings = {
@@ -31,6 +33,10 @@ resource "azurerm_linux_web_app" "example" {
   }
 
   https_only = true
+
+  identity {
+    type = "SystemAssigned"
+  }
 
   lifecycle {
     ignore_changes = [
