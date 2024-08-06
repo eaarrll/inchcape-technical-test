003bc820a77b773ad7a5d69e32f84d7fb3af8607
Earl Sioson <sioson.emc@gmail.com>
2024-08-06 09:43:57 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,18 @@
 terraform {
+  required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.59.0"
+      }
+    }
   backend "azurerm" {
     resource_group_name   = "inchcape-rg"
     storage_account_name  = "inchcapesa"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
+}
+provider "azurerm" {
+  use_oidc = true
 }
 
