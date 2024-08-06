1d8e274b183e335eccad76e8331235ab211e4428
Earl Sioson <sioson.emc@gmail.com>
2024-08-06 09:36:22 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,12 +4,6 @@ terraform {
     storage_account_name  = "inchcapesa"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
-
-    client_id             = var.client_id
-    client_secret         = var.client_secret
-    subscription_id       = var.subscription_id
-    tenant_id             = var.tenant_id
-
   }
 }
 
