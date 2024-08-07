e80f6c6285589bd7d4a0adf8654eb97c1973622c
Earl Sioson <sioson.emc@gmail.com>
2024-08-07 20:44:51 +0200

    Update app.test.js

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
 
-test('GET /api/posts', async () => {
-  const response = await axios.get('http://localhost:3000/posts');
+test('GET /', async () => {
+  const response = await axios.get('http://localhost:3000/');
   expect(response.status).toBe(200);
-  expect(response.data).toBeDefined();
-  expect(Array.isArray(response.data)).toBe(true);
 });
 
