cb0cea971126a465570e9469bc3e24e9b564ad58
root <root@DESKTOP-OGLE4UA>
2024-08-07 10:50:30 +0200

    Update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,6 +6,7 @@ terraform {
     tenant_id             = "448a85c3-3457-421d-98bb-a064ea7a8d8c"
     subscription_id       = "d2363feb-a5e7-4392-819e-69ddc191b41c"
     resource_group_name   = "inchcape-rg"
+    use_oidc              = true
   }
 }
 
