9e137aa6b33491fcc51d9b3c3a81f64e283e2de5
Earl Sioson <sioson.emc@gmail.com>
2024-08-08 06:19:31 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name  = "inchcapesa"
     container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    key                   = "${var.environment}.terraform.tfstate"
     tenant_id             = "448a85c3-3457-421d-98bb-a064ea7a8d8c"
     subscription_id       = "d2363feb-a5e7-4392-819e-69ddc191b41c"
     resource_group_name   = "inchcape-rg"
